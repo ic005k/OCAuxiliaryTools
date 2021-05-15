3fb8176e71531a8f8689d75416c31df7213ebfb1
ic005k <zh99889@126.com>
2021-05-15 19:25:02 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,13 +9,11 @@ MainWindow* mw_one;
 
 int main(int argc, char* argv[])
 {
-#ifdef Q_OS_LINUX
-
-#if (QT_VERSION >= QT_VERSION_CHECK(5, 15, 0))
-    //qputenv("QT_ENABLE_HIGHDPI_SCALING", "1");
-    //QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
-#endif
-
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0))
+    {
+        QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+        QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+    }
 #endif
 
     // QApplication a(argc, argv);
