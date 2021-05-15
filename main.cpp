46be375db1975d372b3b946a2accd374924149cc
ic005k <zh99889@126.com>
2021-05-15 08:38:35 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -12,11 +12,8 @@ int main(int argc, char* argv[])
 #ifdef Q_OS_LINUX
 
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 15, 0))
-
-    qputenv("QT_ENABLE_HIGHDPI_SCALING", "1");
-    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(
-        Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
-
+    //qputenv("QT_ENABLE_HIGHDPI_SCALING", "1");
+    //QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
 #endif
 
 #endif
