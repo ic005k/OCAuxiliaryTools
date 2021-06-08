0f3989582d1c4e12b11894ee21ac5b709a6a25d3
ic005k <zh99889@126.com>
2021-06-08 08:01:42 +0800

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -41,7 +41,7 @@ MainWindow::MainWindow(QWidget* parent)
 
     loadLocal();
 
-    CurVerison = "20210606";
+    CurVerison = "20210608";
     ocVer = "0.7.0";
     title = "OC Auxiliary Tools   " + ocVer + " - " + CurVerison + "[*] ";
     setWindowTitle(title);
