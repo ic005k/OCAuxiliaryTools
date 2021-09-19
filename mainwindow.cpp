d180b244437e5c5f1e4e159a39c741c935a6af54
ic005k <zh99889@126.com>
2021-09-19 12:06:14 +0800

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -32,7 +32,7 @@ QVector<QCheckBox*> chk_PickerAttributes;
 QVector<QCheckBox*> chk_ExposeSensitiveData;
 QVector<QCheckBox*> chk_Target;
 
-QString CurVerison = "20210917";
+QString CurVerison = "20210919";
 QString ocVer = "0.7.3";
 
 MainWindow::MainWindow(QWidget* parent)
