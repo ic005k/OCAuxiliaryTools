a6eab84c8b7a2fd3d618eb9fcdaa8d4916f3866a
ic005k <zh99889@126.com>
2021-09-25 16:17:34 +0800

    Add files via upload

--- autoupdatedialog.cpp
+++ autoupdatedialog.cpp
@@ -10,6 +10,15 @@ AutoUpdateDialog::AutoUpdateDialog(QWidget* parent)
 {
     ui->setupUi(this);
 
+    QFileInfo appInfo(qAppName());
+    qDebug() << appInfo.absoluteFilePath();
+    qDebug() << appInfo.path();
+    qDebug() << appInfo.absolutePath();
+    qDebug() << appInfo.canonicalFilePath();
+    qDebug() << appInfo.canonicalPath();
+    qDebug() << appInfo.filePath();
+
+
     setWindowTitle("");
     ui->progressBar->setTextVisible(false);
     Init();
