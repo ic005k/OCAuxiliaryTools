8b2f6f0c9fc5b8532a4f622d4060965b5ac20dc5
ic005k <zh99889@126.com>
2021-10-13 15:14:08 +0800

    Add files via upload

--- aboutdialog.cpp
+++ aboutdialog.cpp
@@ -4,7 +4,7 @@
 #include "ui_aboutdialog.h"
 
 extern MainWindow* mw_one;
-QString CurVerison = "20211015";
+QString CurVerison = "20211016";
 QString ocVer = "0.7.5";
 
 aboutDialog::aboutDialog(QWidget* parent)
