907bb178364b27869f2465f22fc020f6d620a587
ic005k <zh99889@126.com>
2021-11-30 22:00:40 +0800

    Add files via upload

--- tooltip.cpp
+++ tooltip.cpp
@@ -1,115 +1,125 @@
 #include "tooltip.h"
-#include "mainwindow.h"
+
 #include <QAbstractTextDocumentLayout>
 #include <QDebug>
 #include <QEvent>
 #include <QHBoxLayout>
 #include <QLabel>
 
+#include "mainwindow.h"
+
 extern MainWindow* mw_one;
 
-Tooltip::Tooltip(QWidget* parent)
-    : QDialog(parent)
-{
+Tooltip::Tooltip(QWidget* parent) : QDialog(parent) {
+  this->setAutoFillBackground(true);
+  QPalette palette = this->palette();
+  palette.setColor(QPalette::Base, QColor(255, 255, 205, 0));
+  palette.setColor(QPalette::Window, QColor(255, 255, 205, 0));
+  palette.setColor(QPalette::Text, QColor(0, 0, 0, 255));
+  this->setPalette(palette);
 
-    this->setAutoFillBackground(true); // 属性控制小部件背景是否自动填充
-    QPalette palette = this->palette();
-    palette.setColor(QPalette::Base, QColor(255, 255, 205, 160));
-    palette.setColor(QPalette::Window, QColor(255, 255, 205, 160));
-    palette.setColor(QPalette::Text, QColor(0, 0, 0, 255));
-    this->setPalette(palette);
+  setWindowFlags(Qt::FramelessWindowHint | Qt::Dialog);
+  setAttribute(Qt::WA_DeleteOnClose);
 
-    setWindowFlags(Qt::FramelessWindowHint | Qt::Dialog);
-    setAttribute(Qt::WA_DeleteOnClose);
+  edit = new QTextEdit(this);
+  edit->setWordWrapMode(QTextOption::WordWrap);
+  edit->setReadOnly(true);
 
-    edit = new QTextEdit(this);
-    edit->setWordWrapMode(QTextOption::WordWrap);
-    edit->setReadOnly(true);
+  QHBoxLayout* mLayout = new QHBoxLayout(this);
+  mLayout->setMargin(0);
+  mLayout->addWidget(edit);
 
-    QHBoxLayout* mLayout = new QHBoxLayout(this);
-    mLayout->setMargin(0);
-    mLayout->addWidget(edit);
+  this->installEventFilter(this);
 
-    this->installEventFilter(this);
-    //this->setStyleSheet("QWidget { background: rgba(255 ,248 ,220 ,160); color: black}");
-
-    thisWidth = 500;
-    thisHeight = 250;
+  thisWidth = 500;
+  thisHeight = 250;
 
 #ifdef Q_OS_WIN32
-    thisWidth = 600;
-    thisHeight = 300;
+  thisWidth = 600;
+  thisHeight = 300;
 #endif
 
-    this->resize(thisWidth, thisHeight);
+  this->resize(thisWidth, thisHeight);
 }
 
-Tooltip::~Tooltip()
-{
-}
+Tooltip::~Tooltip() {}
 
-void Tooltip::setMyText(QString strHead, const QString& text)
-{
-    QString str;
-    QStringList strList;
+void Tooltip::setMyText(QString strHead, const QString& text) {
+  QString str;
+  QStringList strList;
 
-    if (text.contains("----")) {
-        strList = text.split("----");
-        if (mw_one->zh_cn) {
-            str = strList.at(1);
-        } else
-            str = strList.at(0);
+  if (text.contains("----")) {
+    strList = text.split("----");
+    if (mw_one->zh_cn) {
+      str = strList.at(1);
     } else
-        str = text;
+      str = strList.at(0);
+  } else
+    str = text;
 
-    edit->setText(strHead + str.trimmed());
+  edit->setText(strHead + str.trimmed());
 
-    // 文本高度
-    QTextDocument* document = edit->document(); //new QTextDocument(edit);
-    document->setTextWidth(thisWidth);
-    QTextOption op;
-    op.setWrapMode(QTextOption::WordWrap);
-    document->setDefaultTextOption(op);
-    document->adjustSize();
-    QAbstractTextDocumentLayout* layout = document->documentLayout();
+  // 文本高度
+  QTextDocument* document = edit->document();  // new QTextDocument(edit);
+  document->setTextWidth(thisWidth);
+  QTextOption op;
+  op.setWrapMode(QTextOption::WordWrap);
+  document->setDefaultTextOption(op);
+  document->adjustSize();
+  QAbstractTextDocumentLayout* layout = document->documentLayout();
 
-    int newHeight = layout->documentSize().height();
+  int newHeight = layout->documentSize().height();
 
-    if (newHeight < thisHeight)
-        currentHeight = newHeight;
-    else
-        currentHeight = thisHeight;
+  if (newHeight < thisHeight)
+    currentHeight = newHeight;
+  else
+    currentHeight = thisHeight;
 
-    edit->setFixedHeight(currentHeight * 1.05);
-    this->setFixedHeight(currentHeight * 1.05);
+  edit->setFixedHeight(currentHeight * 1.05);
+  this->setFixedHeight(currentHeight * 1.05);
 }
 
-bool Tooltip::eventFilter(QObject* obj, QEvent* e)
-{
-    if (obj == this) {
-        if (QEvent::WindowDeactivate == e->type()) {
-            this->close();
-            e->accept();
-            return true;
-        }
+bool Tooltip::eventFilter(QObject* obj, QEvent* e) {
+  if (obj == this) {
+    if (QEvent::WindowDeactivate == e->type()) {
+      this->close();
+      e->accept();
+      return true;
     }
-    return QWidget::eventFilter(obj, e);
+  }
+  return QWidget::eventFilter(obj, e);
 }
 
-void Tooltip::popup(QPoint pos, QString strHead, const QString& text)
-{
-    Tooltip* t = new Tooltip();
-    t->setMyText(strHead, text);
+void Tooltip::popup(QPoint pos, QString strHead, const QString& text) {
+  Tooltip* t = new Tooltip();
+  t->setMyText(strHead, text);
 
-    int newX;
-    if (pos.x() + thisWidth > mw_one->getMainWidth())
-        newX = pos.x() - thisWidth;
-    else
-        newX = pos.x();
+  int newX;
+  if (pos.x() + thisWidth > mw_one->getMainWidth())
+    newX = pos.x() - thisWidth;
+  else
+    newX = pos.x();
 
-    pos.setY(pos.y() - t->height());
-    pos.setX(newX);
+  pos.setY(pos.y() - t->height());
+  pos.setX(newX);
 
-    t->move(pos);
-    t->show();
+  t->move(pos);
+  t->show();
+}
+
+void Tooltip::paintEvent(QPaintEvent* event) {
+  QPainter painter(this);
+  painter.setRenderHint(QPainter::Antialiasing);
+  painter.setBrush(QBrush(Qt::lightGray));
+  painter.setPen(Qt::transparent);
+  QRect rect = this->rect();
+  rect.setWidth(rect.width() - 1);
+  rect.setHeight(rect.height() - 1);
+  // painter.drawRoundedRect(rect, 15, 15);
+
+  QPainterPath painterPath;
+  painterPath.addRoundedRect(rect, 10, 10);
+  painter.drawPath(painterPath);
+
+  QWidget::paintEvent(event);
 }
--- tooltip.h
+++ tooltip.h
@@ -1,30 +1,34 @@
 #ifndef TOOLTIP_H
 #define TOOLTIP_H
 
+#include <QBitmap>
 #include <QDialog>
+#include <QPainterPath>
 #include <QTextBrowser>
 #include <QTextEdit>
 
 class QLabel;
 
 class Tooltip : public QDialog {
-public:
-    Tooltip(QWidget* parent = nullptr);
-    ~Tooltip();
+ public:
+  Tooltip(QWidget* parent = nullptr);
+  ~Tooltip();
 
-    void setMyText(QString strHead, const QString& text);
-    void popup(QPoint pos, QString strHead, const QString& text);
+  void setMyText(QString strHead, const QString& text);
+  void popup(QPoint pos, QString strHead, const QString& text);
 
-protected:
-    bool eventFilter(QObject* obj, QEvent* e);
+ protected:
+  bool eventFilter(QObject* obj, QEvent* e) override;
 
-private:
-    QTextEdit* edit;
-    int thisWidth = 0;
-    int thisHeight = 0;
-    int currentHeight = 0;
+  void paintEvent(QPaintEvent* event) override;
 
-private slots:
+ private:
+  QTextEdit* edit;
+  int thisWidth = 0;
+  int thisHeight = 0;
+  int currentHeight = 0;
+
+ private slots:
 };
 
-#endif // TOOLTIP_H
+#endif  // TOOLTIP_H
