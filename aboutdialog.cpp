8220d0825041870b4c7252b02130f2ebd8110c8e
ic005k <zh99889@126.com>
2022-01-24 10:29:27 +0800

    Add files via upload

--- aboutdialog.cpp
+++ aboutdialog.cpp
@@ -5,7 +5,7 @@
 #include "ui_mainwindow.h"
 
 extern MainWindow* mw_one;
-QString CurVerison = "20220079";
+QString CurVerison = "20220080";
 QString ocVer = "0.7.7";
 QString ocVerDev = "0.7.8";
 QString ocFrom, ocFromDev, strOCFrom, strOCFromDev;
