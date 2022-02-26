b4ee7f88bb4731cd4988b1b6aba58faa167dfe63
ic005k <zh99889@126.com>
2022-02-26 09:13:42 +0800

    Add files via upload

--- aboutdialog.cpp
+++ aboutdialog.cpp
@@ -7,7 +7,7 @@
 
 extern MainWindow* mw_one;
 extern QString strAppName, strIniFile;
-QString CurVersion = "20220195";
+QString CurVersion = "20220196";
 QString ocVer = "0.7.8";
 QString ocVerDev = "0.7.9";
 QString ocFrom, ocFromDev, strOCFrom, strOCFromDev;
