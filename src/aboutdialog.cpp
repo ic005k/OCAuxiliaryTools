c1e8e69b25d6a1bcd8065eebf7b9e9b73ffb5401
ic005k <zh99889@126.com>
2023-02-14 21:30:28 +0800

    Update aboutdialog.cpp

--- src/aboutdialog.cpp
+++ src/aboutdialog.cpp
@@ -7,7 +7,7 @@
 
 extern MainWindow* mw_one;
 extern QString strAppName, strIniFile;
-QString CurVersion = "20230015";
+QString CurVersion = "20230016";
 QString ocVer = "0.8.8";
 QString ocVerDev = "0.8.9";
 QString ocFrom, ocFromDev, strOCFrom, strOCFromDev;
