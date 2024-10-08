14c8dac564567955d98da326dc43afd6aee97036
ic005k <zh99889@126.com>
2024-10-08 10:04:50 +0800

    Some tests.

--- src/aboutdialog.cpp
+++ src/aboutdialog.cpp
@@ -7,7 +7,7 @@
 
 extern MainWindow* mw_one;
 extern QString strAppName, strIniFile;
-QString CurVersion = "20240001";
+QString CurVersion = "20240002";
 QString ocVer = "0.8.8";
 QString ocVerDev = "0.8.9";
 QString ocFrom, ocFromDev, strOCFrom, strOCFromDev;
